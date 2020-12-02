eb2936ff490a2d6afb3fb4d38b7eb14f03d21a4e
I-Titovets <74862119+I-Titovets@users.noreply.github.com>
2020-12-02 13:06:27 +0300

    gulpfile
    иду к тому что заработает gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,26 @@
-const gulp = require('gulp');
+const {src, dest, watch} = require('gulp');
 const browserSynk = require('browser-sync').create();
+const sass = require('gulp-sass');
 
-gulp.task('hello', function(done) {
- console.log('Привет, мир!');
- done();
-});
 
 // Static server
-gulp.task('browser-sync', function() {
+function bs() {
+  serveSass();
   browserSync.init({
       server: {
           baseDir: "./"
       }
   });
-});
+  watch("./*.html").on('change', browserSynk.reload);
+  watch("./sass/**/*.sass", serveSass);
+  watch("./js/*.js").on('change', browserSynk.reload);
+};
+
+function serveSass() {
+  return src("./sass/*.sass")
+      .pipe(sass())
+      .pipe(dest("./css"))
+      .pipe(browserSync.stream());
+};
+
+exports.serve = bs;
